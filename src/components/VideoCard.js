00498a33ee6e625313d63bd434204b04d98f9d12
Harshul Roy <harshulroykc@gmail.com>
2024-08-28 11:45:53 +0530

    first commit

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -3,7 +3,7 @@ import React from 'react'
 const VideoCard = ({info}) => {
     
   return (
-    <div className='p-2 w-[22rem]'>
+    <div className='p-2 w-[100%]'>
       <img className='rounded-lg' src={info?.snippet?.thumbnails?.medium?.url} alt="thumbnail" />
       <ul>
         <li className='font-bold py-2'>{info?.snippet?.title}</li>
--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -3,25 +3,26 @@ import { YOUTUBE_VIDEOS_API } from '../utils/constant'
 import VideoCard from './VideoCard'
 import { Link } from 'react-router-dom'
 
+
 const VideContainer = () => {
 
   const [videos,setVideos] = React.useState([])
   const getVideos = async()=>{
     try {
       let data = await fetch(YOUTUBE_VIDEOS_API)
-    data = await data.json()
+    data = await data.json() 
     setVideos(data.items)
     } catch (error) {
-      console.log(error)
+      console.log('video container error',error)
     }
   }
   React.useEffect(()=>{
     getVideos()
   },[])
 
-  return videos.length===0?<div></div>: (
+  return videos.length===0?<div><img className='w-[66%] h-auto m-auto' src={process.env.PUBLIC_URL+'/Poor-Internet.png'} alt="noInternet" /></div>: (
     <div className='flex flex-wrap m-6'>
-      {videos.map((item)=> <Link to={"/watch?v="+item.id} key={item.id}><VideoCard info={item}/></Link>)}
+      {videos.map((item)=> <Link to={"/watch?v="+item.id} key={item.id} className='w-[22rem]'><VideoCard info={item}/></Link>)}
     </div>
   )
 }
