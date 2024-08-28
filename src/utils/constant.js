73ab3fe36027a95ba94ea33e92d9719d959524ce
Harshul Roy <harshulroykc@gmail.com>
2024-08-28 16:33:43 +0530

    second commit

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,6 +1,6 @@
 let apikey = process.env.REACT_APP_API_KEY 
 export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+apikey;
-export const YOUTUBE_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=chrome&ds=yt&q="
+export const YOUTUBE_SEARCH_API = "https://suggestqueries.google.com/complete/search?client=chrome&ds=yt&q="
 
 const COMMENT_API = `https://www.googleapis.com/youtube/v3/commentThreads?key=${apikey}&textFormat=plainText&part=snippet&maxResults=50&videoId=`
 
